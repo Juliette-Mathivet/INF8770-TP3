4384006da6ffb437e90ee08a5ff4ebdac573791a
Kevin Peymani <kevin.peymani@polymtl.ca>
2024-04-02 16:49:47 -0400

    ca avance bien for now (foreshadowing)

--- src/histogramme.py
+++ src/histogramme.py
@@ -1,15 +1,36 @@
 from PIL import Image
 from IPython.display import display
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 
+# image_path = "../data/jpeg/i000.jpeg"
+# image = Image.open(image_path)
+# display(image)
 
-image_path = "RGB.jpg"
-image = Image.open(image_path)
-display(image)
+def obtenirDescHistogrammeCouleur(image): 
 
-# conversion de l'image en 3 tableaux R,G,B
+    # conversion de l'image en 3 tableaux R,G,B
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    RGBArray = np.asarray(image)
+    Rarray = []
+    Garray = []
+    Barray = []
+    for pixel in RGBArray:
+        Rarray = np.append(Rarray, pixel[0])
+        Garray = np.append(Garray, pixel[1])
+        Barray = np.append(Barray, pixel[2])
 
-# conversion 256 -> 32 niveaux
+    # conversion 256 -> 32 niveaux et création des histogrammes
+    histoR = np.histogram(Rarray, 32, (0.0, 256.0))
+    histoG = np.histogram(Garray, 32, (0.0, 256.0))
+    histoB = np.histogram(Barray, 32, (0.0, 256.0))
 
-# quantification
+    # concaténation des 3 quantifications en un seul vecteur de dimension D
+    histogramme = np.append(histoR[0], [histoG[0], histoB[0]])
 
-# concaténation des 3 quantifications en un seul vecteur de dimension D
+    plt.plot(histogramme)
+    plt.title(f"Descripteur d'une image à partir d'un histogramme de couleur")
+    plt.show()
+
+    return(histogramme)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,65 @@
+import numpy as np
+import cv2
+import sys
+from histogramme import *
+from reseauNeurones import *
 
+global descriptor
+descriptor = ""
 
+def distanceEuclidienne(array1, array2):
+    return np.linalg.nomr(array1 - array2)
+
+#https://www.futurelearn.com/info/courses/introduction-to-image-analysis-for-plant-phenotyping/0/steps/305359
+def obtenirFrame(path, timestamp):
+    video = cv2.VideoCapture(path)
+    video.set(cv2.CAP_PROP_POS_MSEC, timestamp)
+    ret, frame = video.read()
+    #cv2.imshow('frame', frame)
+    #cv2.waitKey(0)
+    return frame
+
+# https://www.geeksforgeeks.org/get-video-duration-using-python-opencv/
+def longueurVideo(path):
+    video = cv2.VideoCapture(path)
+    frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = video.get(cv2.CAP_PROP_FPS)
+    seconds = round(frames / fps)
+    return seconds
 
 # indexation
+def indexationFrames(path):
+    global descriptor
+    longueur = longueurVideo(path)
+    frames = {}
+    i = 0
+    while(i < longueur):
+        if(descriptor == 'H'):
+            frames[i] = (obtenirDescHistogrammeCouleur(obtenirFrame(path, i*1000)))
+        elif(descriptor == 'R'):
+            frames[i] = (obtenirDescReseauNeurones(obtenirFrame(path, i*1000)))
+        i += 1
+    return frames
+
+
+
+
+#TODO path dymanique
 
 # recherche avec NN ou Histo
 
-# écriture dans results/test.csv
+def main():
+    global descriptor
+    descriptor = sys.argv[1]
+    # R : option pour Réseau Neuronal, H : option pour histogramme de couleur
+    while(descriptor != 'R' and descriptor != 'H'):
+        descriptor = input("Veuillez entrer un choix de descripteur valide (R ou H) \n")
+    print(indexationFrames("../data/mp4/v001.mp4"))
+
+
+#ouvrir jpeg avec opencv
+
+
+
+if __name__ == "__main__":
+    main()
